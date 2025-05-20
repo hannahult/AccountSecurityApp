a47e280e36d728ac39feaa024c6c2399eb192e64
Hanna Hult <hannaaaa31@gmail.com>
2025-05-20 14:47:57 +0200

    Added API Added Models folder Added class for database; User

---
+++ AccountSecurityApp.API/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccountSecurityApp.API.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(70)]
+        public string Username { get; set; } = ""; 
+        public string PasswordHash { get; set; } = ""; 
+    }
+}
