2344037f2f27ef3dd20b2f41ad8a13b59999387d
Hanna Hult <hannaaaa31@gmail.com>
2025-05-22 13:26:59 +0200

    Added AccountController Added register endpoint

---
+++ AccountSecurityApp.API/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using AccountSecurityApp.API.Data;
+using AccountSecurityApp.API.DTOs;
+using AccountSecurityApp.API.Models;
+using BCrypt.Net;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountSecurityApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+
+        public AccountController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDTO request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Username) || string.IsNullOrWhiteSpace(request.Password))
+                return BadRequest("Användarnamn och lösenord krävs.");
+
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            var user = new User
+            {
+                Username = request.Username,
+                PasswordHash = passwordHash
+            };
+
+            _dbContext.Users.Add(user);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok("Registrering lyckades");
+        }
+    }
+}
