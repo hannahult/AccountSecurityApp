c87bbbd508aff55c0913df280c81a2fb3dc56e57
Hanna Hult <hannaaaa31@gmail.com>
2025-06-09 10:55:59 +0200

    Added UserService interface

---
+++ AccountSecurityApp.API/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using AccountSecurityApp.API.DTOs;
+
+namespace AccountSecurityApp.API.Services
+{
+    public interface IUserService
+    {
+        Task<bool> RegisterAsync(RegisterRequestDTO dto);
+        Task<string?> LoginAsync(LoginRequestDTO dto);
+    }
+}
